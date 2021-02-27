da2022720dfee1c6733da69fa73a908b61996a20
imyofather79 <chunsum708@gmail.com>
2021-02-26 17:43:42 -0700

    commit tree df7b45f0ee64a00391f48dec901e2046df935405

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
+    array = ["I", "wow"]
+    array.sort
 
 end
 
 def using_reverse(array)
-
+    array = ["wow", "arrays!"]
+    array.reverse
 end
 
 def using_first(array)
-
+    array = ["wow", "no", "yes"]
+    array.first
 end
 
 def using_last(array)
-
+    array = ["wow", "no", "yes", "arrays!"]
+    array.last
 end
 
 def using_size(array)
-
+    array = ["wow", "no", "yes", "arrays!", "hi", "maybe"]
+    array.size
 end
